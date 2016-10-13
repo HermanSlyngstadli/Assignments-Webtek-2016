c73e633c13b24881a2ac1512027a6e9742fc68a4
Herman <herman@slyngstadli.no>
2016-10-13 13:25:20 +0200

    Added comments and check for errors

--- Ass 5/js/script.js
+++ Ass 5/js/script.js
@@ -1,6 +1,6 @@
 /* Part 2 */
 console.log('PART 2');
-
+// Starts at 1 and stops at 20
 for(var i = 1; i < 21; i++)  {
 	console.log(i);
 }
@@ -10,21 +10,22 @@ console.log('PART 3');
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
+// Cycles through the numbers in the array, and checks its divisibility
 for (var i = 0; i < numbers.length; i++) {
 	if (numbers[i] % 5 == 0 && numbers[i] % 3 == 0) {
-		console.log('eplekake');
+		console.log('eplekake'); // Stops here if the number is divisible by both 5 and 3
 	} else if (numbers[i] % 5 == 0) {
-		console.log('kake');
+		console.log('kake');	// Stops here if the number is divisible by both 5
 	} else if (numbers[i] % 3 == 0) {
-		console.log('eple');
+		console.log('eple'); // Stops here if the number is divisible by both 3
 	} else {
-		console.log(numbers[i]);
+		console.log(numbers[i]); // If there is no special case, it just logs the number
 	}
 }
 
 /* Part 4 */
 var title = document.getElementById('title');
-title.innerHTML = 'Hello, JavaScript';
+title.innerHTML = 'Hello, JavaScript. Wadduuup?!'; // wubba lubba dub dub!!!!
 /* Part 5 */
 var magic = document.getElementById('magic');
 
@@ -61,10 +62,11 @@ const technologies = [
 
 var techytech = document.getElementById('tech');
 
+// Cycle through technologies and push it to the list as a new element
 for (var i = 0; i < technologies.length; i++) {
 	techytech.innerHTML += '<li>' + technologies[i] + '</li>';
 	/*
-	Eventuetlt
+	Alternatively
 	
 	var lisp = document.createElement('li');
 	var innhold = document.createTextNode(technologies[i]);
