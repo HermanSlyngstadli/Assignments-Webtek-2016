8bb8ad185a39656e9d542bc160bf36505884f711
Herman <herman@slyngstadli.no>
2016-10-30 14:22:37 +0100

    Added comments to ass 7

--- Ass 7/todo.js
+++ Ass 7/todo.js
@@ -5,6 +5,7 @@
 
 */
 
+// Init
 var tasks = [];
 var list = document.getElementById('taskList');
 var output = document.getElementsByTagName('output')[0];
@@ -17,50 +18,49 @@ function task(timestamp, description) {
 }
 
 window.onload = function() {
-	// 
+	// Add event listener to button when the site is fully loaded
 	document.getElementById('addButton').addEventListener('click', function() {
 		addTask();
 	});
 }
 
 function addTask() {
-	//
+	// Make the checkbox
 	var check = document.createElement('input');
 	check.type = 'checkbox';
 
-	//
+	// Add event listener to check box
 	check.addEventListener('change', function() {
 		changeStatus(this);
 	});
 
-	//
+	// Make input field
 	var inputText = document.getElementById('inputField').value;
 	var taskDescription = document.createTextNode(inputText);
 
-	//
+	// Make list element
 	var newListElement = document.createElement('li');
 
-	//
+	// Append checkbox and text to list element
 	newListElement.appendChild(check);
 	newListElement.appendChild(taskDescription);
 
-	//
+	// Make a new task from object and push it to the array
 	var newTask = new task(Date.now(), taskDescription);
 	tasks.push(newTask);
 
-	//
+	// Set id of checkbox to timestamp of task and append to list
 	check.id = newTask.timestamp;
 	list.appendChild(newListElement);
 
-	//
+	// Update the output field
 	updateCounter();
 }
 
 function changeStatus(checkbox) {
-	//
 	var checkIndex = findIndex(checkbox.id);
 
-	//
+	// Set or remove line-through
 	if (list.children[checkIndex].style.textDecoration == 'line-through') {
 		list.children[checkIndex].style.textDecoration = 'none';
 		updateCounter('remove');
@@ -71,7 +71,7 @@ function changeStatus(checkbox) {
 }
 
 function findIndex(checkId) {
-	//
+	// Finding index that mathces id of checkbox field
 	for (var i = 0; i < tasks.length; i++) {
 		if (tasks[i].timestamp == checkId) {
 			return i;
@@ -80,13 +80,13 @@ function findIndex(checkId) {
 }
 
 function updateCounter(command) {
-	//
+	// add or subtract from completeCounter
 	if (command == 'add') {
 		completeCounter++;
 	} else if (command == 'remove') {
 		completeCounter--;
 	}
 	
-	//
+	// Update output field
 	output.innerHTML = completeCounter + '/' + tasks.length;
 }
